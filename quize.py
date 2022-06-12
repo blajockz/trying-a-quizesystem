2dd0e1bf34d21c868631dd4da90d309ead9e8e58
blajockz <69370048+blajockz@users.noreply.github.com>
2022-06-12 20:53:58 +0100

    Update quize.py

--- quize.py
+++ quize.py
@@ -1,74 +1,157 @@
 
-
-a = "What is the smallest country in the world with an area of only 0.49 square kilometres?"
-b = "Which of the following is a landlocked country, meaning it is not bordered by the sea or ocean?"
-c = "What is the second-largest country in the world after Russia?"
-d = "Which of the following countries is not in Central America?"
-e = "Which of the following countries has the largest Muslim population in the world?"
-f = "Which European country is divided into administrative divisions called departments?"
-g = "Which of the following countries is shaped like a high heel boot?"
-h = "Mouth Kilimanjaro, the highest mountain in Africa, is located in which country?"
-i = "In which country can you visit Machu Picchu, which used to be the citadel of the Inca Empire?"
-j = " Pho, which contains beef, herbs, rice noodles, and broth, is a traditional dish in which Asian country?"
-
-print("What is the smallest country in the world with an area of only 0.49 square kilometres?")
-ans = input()
-if ans == "Vatican":
-    print("Your answer is correct!")
-else:
-    print("Your answer is wrong.")
-print("Which of the following is a landlocked country, meaning it is not bordered by the sea or ocean?\na)Turkey ; b)Madagascar ; c)Kazakhstan")
-res = input()
-if res == "Kazakhstan":
-    print("Your answer is correct")
-else:
-    print("Your answer is wrong.")
-print("What is the second-largest country in the world after Russia?")
-value = input()
-if value == "Canada":
-    print("Your answer is correct!")
-else:
-    print("Your answer is wrong")
-print("Which of the following countries is not in Central America?\na)Cuba ; b)Mexico ; c)Uruguay")
-resp = input()
-if resp == "Uruguay":
-    print("Your answer is correct!")
-else:
-    print("Your answer is wrong.")
-print("Which of the following countries has the largest Muslim population in the world?\na)Saudi Arabia ; b)Indonesia ; c)Iran")
-country = input()
-if country == "Indonesia":
-    print("Your answer is correct!")
-else:
-    print("Your answer is wrong.")
-print("Which European country is divided into administrative divisions called departments?\na)France ; b)Spain ; c)Portugal")
-dep = input()
-if dep == "France":
-    print("Your answer is correct!")
-else:
-    print("Your answer is wrong.")
-print("Which of the following countries is shaped like a high heel boot?\na)Chile ; b)Finland , c)italy")
-boot = input()
-if boot == "italy" or "Italy":
-    print("Your answer is correct")
-else:
-    print("Your answer is wrong.")
-print("Mouth Kilimanjaro, the highest mountain in Africa, is located in which country?\na)Lesotho ; b)Tanzania ; c)Kenya")
-kili = input()
-if kili == "Tanzania":
-    print("Your answer is correct!")
-else:
-    print("Your answer is wrong.")
-print("In which country can you visit Machu Picchu, which used to be the citadel of the Inca Empire?\na)Bolivia ; b)Peru ; c)Guatemala")
-cita = input()
-if cita == "Peru":
-    print("Your answer is correct!")
-else:
-    print("Your answer is wrong.")
-print("Pho, which contains beef, herbs, rice noodles, and broth, is a traditional dish in which Asian country?\na)Cambodia ; b)Vietnam ; c)Malaysia")
-brot = input()
-if brot == "Vietnam":
-    print("Your answer is correct:")
-else:
-    print("Your answer is wrong.")
-#endif
+print("Welcome to the QuizeScience")
+print("Please choose a system : a)Maths ; b)Soccer ; c)Countries")
+choice = input()
+if choice == "Countries":
+    print("What is the smallest country in the world with an area of only 0.49 square kilometres?")
+    ans = input()
+    if ans == "Vatican":
+        print("Your answer is correct!")
+    else:
+        print("Your answer is wrong.")
+    print("Which of the following is a landlocked country, meaning it is not bordered by the sea or ocean?\na)Turkey ; b)Madagascar ; c)Kazakhstan")
+    res = input()
+    if res == "Kazakhstan":
+        print("Your answer is correct")
+    else:
+        print("Your answer is wrong.")
+    print("What is the second-largest country in the world after Russia?")
+    value = input()
+    if value == "Canada":
+        print("Your answer is correct!")
+    else:
+        print("Your answer is wrong")
+    print("Which of the following countries is not in Central America?\na)Cuba ; b)Mexico ; c)Uruguay")
+    resp = input()
+    if resp == "Uruguay":
+        print("Your answer is correct!")
+    else:
+        print("Your answer is wrong.")
+    print("Which of the following countries has the largest Muslim population in the world?\na)Saudi Arabia ; b)Indonesia ; c)Iran")
+    country = input()
+    if country == "Indonesia":
+        print("Your answer is correct!")
+    else:
+        print("Your answer is wrong.")
+    print("Which European country is divided into administrative divisions called departments?\na)France ; b)Spain ; c)Portugal")
+    dep = input()
+    if dep == "France":
+        print("Your answer is correct!")
+    else:
+        print("Your answer is wrong.")
+    print("Which of the following countries is shaped like a high heel boot?\na)Chile ; b)Finland , c)italy")
+    boot = input()
+    if boot == "italy" or "Italy":
+        print("Your answer is correct")
+    else:
+        print("Your answer is wrong.")
+    print("Mouth Kilimanjaro, the highest mountain in Africa, is located in which country?\na)Lesotho ; b)Tanzania ; c)Kenya")
+    kili = input()
+    if kili == "Tanzania":
+        print("Your answer is correct!")
+    else:
+        print("Your answer is wrong.")
+    print("In which country can you visit Machu Picchu, which used to be the citadel of the Inca Empire?\na)Bolivia ; b)Peru ; c)Guatemala")
+    cita = input()
+    if cita == "Peru":
+        print("Your answer is correct!")
+    else:
+        print("Your answer is wrong.")
+    print("Pho, which contains beef, herbs, rice noodles, and broth, is a traditional dish in which Asian country?\na)Cambodia ; b)Vietnam ; c)Malaysia")
+    brot = input()
+    if brot == "Vietnam":
+        print("Your answer is correct:")
+    else:
+        print("Your answer is wrong.")
+elif choice == "Maths":
+    print("Who's the first true mathematician and the first known individual to whom a mathematical discovery has been attributed?\na)Thales of Miletus ; b)Isaac Newton ; c)Aflatoun")
+    sc = input()
+    if sc == "Thales of Miletus":
+        print("Your answer is correct!")
+    else:
+        print("Your answer is wrong.")
+    print("When is Pi Day celebrated around the world?")
+    lol=input()
+    if lol=="March 14":
+        print("your answer us correct!")
+    else:
+        print("Your answer is wrong, the answer is March 14")
+    print("Angle greater than 180 degrees but less than 360 degrees are called?")
+    mm=input()
+    if mm=="Reflex Angles":
+        print("your answer is true")
+    else :
+        print ("your answer is wrong, it's Reflex Angles")
+    print("Who discovered the laws of the lever and pulley?")
+    who = input()
+    if who== "Archimedes":
+        print("Your answer is true")
+    else :
+        print("Your answer is wrong, it's Archimedes")
+    print("Scientist who was born on Pi Day?")
+    oo= input()
+    if oo=="Albert Einstein":
+        print("right answer")
+    else:
+        print("your answer is wrong, it's Albert Einstein")
+    print("Who discovered the Symbol Infinity “∞”?")
+    ayo=input()
+    if ayo=="John Wallis":
+        print("right answer")
+    else:
+        print("your answer is wrong, it's John Wallis")
+    print("Who invented Protractor?")
+    rr= input()
+    if rr=="Joseph Huddart":
+        print ("your answer is true")
+    else :
+        print ("your answer is wrong, it's Jose Huddart")
+    print (" Where was Abacus invented?")
+    kku=input()
+    if kku=="China":
+         print("true answer")
+    else:
+         print("wrong answer, it's china")
+elif choice == "Soocer":
+    print("FIFA stands for what?")
+    fifa = input()
+    if fifa == "Federation Initernationale de Football Association":
+        print("Your answer is correct.")
+    else:
+        print("Your answer is wrong, the correct one is Federation Internationale de Fotball Association")
+    print("What is the number of players in 1 team of football?")
+    num = input()
+    if num == "11+ goal keeper" or "12":
+        print("Your answer is correct!")
+    else:
+        print("Your answer is wrong, the correct one is 11 players with the goalkeeper")
+    print("In which country the headquarter of FA located?")
+    hf = input()
+    if hf == "England":
+        print("Your answer is correct!")
+    else:
+        print("Your answer is wrong, the correct one is England")
+    print("Which country won the highest number of world cups?")
+    cup = input()
+    if cup == "Brazil":
+        print("Your answer is correct")
+    else:
+        print("Your answer is wrong, the correct one is Brazil")
+    print("Bulgaria FIFA code is?")
+    code = input()
+    if code == "BUL":
+        print("Your answer is correct")
+    else:
+        print("Your answer is wrong, the correct one is BUL")
+    print("Which footballer has autobiography titled “A goal in the sky”?")
+    auto = input()
+    if auto == "Roberto Baggio" or "robert baggio" or "ROBERT BAGGIO":
+        print("Your answer is correct")
+    else:
+        print("Your answer is wrong, the correct one is Robert Baggio")
+    print("What was the earlier form of soccer?")
+    soc = input()
+    if soc == "Socca" or "socca" or "SOCCA":
+        print("Your answer is correct")
+    else:
+        print("Your answer is wrong, the correct one is Socca")
