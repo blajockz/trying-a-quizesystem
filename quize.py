3dae6ef97e0b7f6bd30c0d70c63a728b01736d89
blajockz <69370048+blajockz@users.noreply.github.com>
2022-06-12 18:38:33 +0100

    Create quize.py

---
+++ quize.py
@@ -0,0 +1,74 @@
+
+
+a = "What is the smallest country in the world with an area of only 0.49 square kilometres?"
+b = "Which of the following is a landlocked country, meaning it is not bordered by the sea or ocean?"
+c = "What is the second-largest country in the world after Russia?"
+d = "Which of the following countries is not in Central America?"
+e = "Which of the following countries has the largest Muslim population in the world?"
+f = "Which European country is divided into administrative divisions called departments?"
+g = "Which of the following countries is shaped like a high heel boot?"
+h = "Mouth Kilimanjaro, the highest mountain in Africa, is located in which country?"
+i = "In which country can you visit Machu Picchu, which used to be the citadel of the Inca Empire?"
+j = " Pho, which contains beef, herbs, rice noodles, and broth, is a traditional dish in which Asian country?"
+
+print("What is the smallest country in the world with an area of only 0.49 square kilometres?")
+ans = input()
+if ans == "Vatican":
+    print("Your answer is correct!")
+else:
+    print("Your answer is wrong.")
+print("Which of the following is a landlocked country, meaning it is not bordered by the sea or ocean?\na)Turkey ; b)Madagascar ; c)Kazakhstan")
+res = input()
+if res == "Kazakhstan":
+    print("Your answer is correct")
+else:
+    print("Your answer is wrong.")
+print("What is the second-largest country in the world after Russia?")
+value = input()
+if value == "Canada":
+    print("Your answer is correct!")
+else:
+    print("Your answer is wrong")
+print("Which of the following countries is not in Central America?\na)Cuba ; b)Mexico ; c)Uruguay")
+resp = input()
+if resp == "Uruguay":
+    print("Your answer is correct!")
+else:
+    print("Your answer is wrong.")
+print("Which of the following countries has the largest Muslim population in the world?\na)Saudi Arabia ; b)Indonesia ; c)Iran")
+country = input()
+if country == "Indonesia":
+    print("Your answer is correct!")
+else:
+    print("Your answer is wrong.")
+print("Which European country is divided into administrative divisions called departments?\na)France ; b)Spain ; c)Portugal")
+dep = input()
+if dep == "France":
+    print("Your answer is correct!")
+else:
+    print("Your answer is wrong.")
+print("Which of the following countries is shaped like a high heel boot?\na)Chile ; b)Finland , c)italy")
+boot = input()
+if boot == "italy" or "Italy":
+    print("Your answer is correct")
+else:
+    print("Your answer is wrong.")
+print("Mouth Kilimanjaro, the highest mountain in Africa, is located in which country?\na)Lesotho ; b)Tanzania ; c)Kenya")
+kili = input()
+if kili == "Tanzania":
+    print("Your answer is correct!")
+else:
+    print("Your answer is wrong.")
+print("In which country can you visit Machu Picchu, which used to be the citadel of the Inca Empire?\na)Bolivia ; b)Peru ; c)Guatemala")
+cita = input()
+if cita == "Peru":
+    print("Your answer is correct!")
+else:
+    print("Your answer is wrong.")
+print("Pho, which contains beef, herbs, rice noodles, and broth, is a traditional dish in which Asian country?\na)Cambodia ; b)Vietnam ; c)Malaysia")
+brot = input()
+if brot == "Vietnam":
+    print("Your answer is correct:")
+else:
+    print("Your answer is wrong.")
+#endif
