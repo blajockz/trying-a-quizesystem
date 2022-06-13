100ad60824c9744ed9fedb95ec70d3e51645509b
blajockz <69370048+blajockz@users.noreply.github.com>
2022-06-13 08:53:52 +0100

    Update quize.py

--- quize.py
+++ quize.py
@@ -1,157 +1,165 @@
 
-print("Welcome to the QuizeScience")
-print("Please choose a system : a)Maths ; b)Soccer ; c)Countries")
+print("\033[1;34;40m...:/Welcome to the QuizeScience\:...")
+print("\033[1;36;40mPlease choose a system : a)Maths ; b)Soocer ; c)Countries")
 choice = input()
 if choice == "Countries":
-    print("What is the smallest country in the world with an area of only 0.49 square kilometres?")
-    ans = input()
-    if ans == "Vatican":
-        print("Your answer is correct!")
+    print("\033[1;33;40mWhat is the smallest country in the world with an area of only 0.49 square kilometres?")
+    ans = input("===> ")
+    if ans == "Vatican" or "VATICAN" or "vatican" or " Vatican":
+        print("\033[1;32;40mYour answer is correct!")
     else:
-        print("Your answer is wrong.")
-    print("Which of the following is a landlocked country, meaning it is not bordered by the sea or ocean?\na)Turkey ; b)Madagascar ; c)Kazakhstan")
-    res = input()
+        print("\033[1;31;40mYour answer is wrong.")
+    print("\033[1;33;40mWhich of the following is a landlocked country, meaning it is not bordered by the sea or ocean?\na)Turkey ; b)Madagascar ; c)Kazakhstan")
+    res = input("===> ")
     if res == "Kazakhstan":
-        print("Your answer is correct")
+        print("\033[1;32;40mYour answer is correct")
     else:
-        print("Your answer is wrong.")
-    print("What is the second-largest country in the world after Russia?")
-    value = input()
+        print("\033[1;31;40mYour answer is wrong.")
+    print("\033[1;33;40mWhat is the second-largest country in the world after Russia?")
+    value = input("===> ")
     if value == "Canada":
-        print("Your answer is correct!")
+        print("\033[1;32;40mYour answer is correct!")
     else:
-        print("Your answer is wrong")
-    print("Which of the following countries is not in Central America?\na)Cuba ; b)Mexico ; c)Uruguay")
-    resp = input()
+        print("\033[1;31;40mYour answer is wrong")
+    print("\033[1;33;40mWhich of the following countries is not in Central America?\na)Cuba ; b)Mexico ; c)Uruguay")
+    resp = input("===> ")
     if resp == "Uruguay":
-        print("Your answer is correct!")
+        print("\033[1;32;40mYour answer is correct!")
     else:
-        print("Your answer is wrong.")
-    print("Which of the following countries has the largest Muslim population in the world?\na)Saudi Arabia ; b)Indonesia ; c)Iran")
-    country = input()
+        print("\033[1;31;40mYour answer is wrong.")
+    print("\033[1;33;40mWhich of the following countries has the largest Muslim population in the world?\na)Saudi Arabia ; b)Indonesia ; c)Iran")
+    country = input("===> ")
     if country == "Indonesia":
-        print("Your answer is correct!")
+        print("\033[1;32;40mYour answer is correct!")
     else:
-        print("Your answer is wrong.")
-    print("Which European country is divided into administrative divisions called departments?\na)France ; b)Spain ; c)Portugal")
-    dep = input()
+        print("\033[1;31;40mYour answer is wrong.")
+    print("\033[1;33;40mWhich European country is divided into administrative divisions called departments?\na)France ; b)Spain ; c)Portugal")
+    dep = input("===> ")
     if dep == "France":
-        print("Your answer is correct!")
+        print("\033[1;32;40mYour answer is correct!")
     else:
-        print("Your answer is wrong.")
-    print("Which of the following countries is shaped like a high heel boot?\na)Chile ; b)Finland , c)italy")
-    boot = input()
+        print("\033[1;31;40mYour answer is wrong.")
+    print("\033[1;33;40mWhich of the following countries is shaped like a high heel boot?\na)Chile ; b)Finland , c)italy")
+    boot = input("===> ")
     if boot == "italy" or "Italy":
-        print("Your answer is correct")
+        print("\033[1;32;40mYour answer is correct")
     else:
-        print("Your answer is wrong.")
-    print("Mouth Kilimanjaro, the highest mountain in Africa, is located in which country?\na)Lesotho ; b)Tanzania ; c)Kenya")
-    kili = input()
+        print("\033[1;31;40mYour answer is wrong.")
+    print("\033[1;33;40mMouth Kilimanjaro, the highest mountain in Africa, is located in which country?\na)Lesotho ; b)Tanzania ; c)Kenya")
+    kili = input("===> ")
     if kili == "Tanzania":
-        print("Your answer is correct!")
+        print("\033[1;32;40mYour answer is correct!")
     else:
-        print("Your answer is wrong.")
-    print("In which country can you visit Machu Picchu, which used to be the citadel of the Inca Empire?\na)Bolivia ; b)Peru ; c)Guatemala")
-    cita = input()
+        print("\033[1;31;40mYour answer is wrong.")
+    print("\033[1;33;40mIn which country can you visit Machu Picchu, which used to be the citadel of the Inca Empire?\na)Bolivia ; b)Peru ; c)Guatemala")
+    cita = input("===> ")
     if cita == "Peru":
-        print("Your answer is correct!")
+        print("\033[1;32;40mYour answer is correct!")
     else:
-        print("Your answer is wrong.")
-    print("Pho, which contains beef, herbs, rice noodles, and broth, is a traditional dish in which Asian country?\na)Cambodia ; b)Vietnam ; c)Malaysia")
-    brot = input()
+        print("\033[1;31;40mYour answer is wrong.")
+    print("\033[1;33;40mPho, which contains beef, herbs, rice noodles, and broth, is a traditional dish in which Asian country?\na)Cambodia ; b)Vietnam ; c)Malaysia")
+    brot = input("===> ")
     if brot == "Vietnam":
-        print("Your answer is correct:")
+        print("\033[1;32;40mYour answer is correct:")
     else:
-        print("Your answer is wrong.")
+        print("\033[1;31;40mYour answer is wrong.")
 elif choice == "Maths":
-    print("Who's the first true mathematician and the first known individual to whom a mathematical discovery has been attributed?\na)Thales of Miletus ; b)Isaac Newton ; c)Aflatoun")
-    sc = input()
+    print("\033[1;33;40mWho's the first true mathematician and the first known individual to whom a mathematical discovery has been attributed?\na)Thales of Miletus ; b)Isaac Newton ; c)Aflatoun")
+    sc = input("===> ")
     if sc == "Thales of Miletus":
-        print("Your answer is correct!")
+        print("\033[1;32;40mYour answer is correct!")
     else:
-        print("Your answer is wrong.")
-    print("When is Pi Day celebrated around the world?")
-    lol=input()
+        print("\033[1;31;40mYour answer is wrong.")
+    print("\033[1;33;40mWhen is Pi Day celebrated around the world?")
+    lol = input("===> ")
     if lol=="March 14":
-        print("your answer us correct!")
+        print("\033[1;32;40mYour answer us correct!")
     else:
-        print("Your answer is wrong, the answer is March 14")
-    print("Angle greater than 180 degrees but less than 360 degrees are called?")
-    mm=input()
+        print("\033[1;31;40mYour answer is wrong, the answer is March 14")
+    print("\033[1;33;40mAngle greater than 180 degrees but less than 360 degrees are called?")
+    mm = input("===> ")
     if mm=="Reflex Angles":
-        print("your answer is true")
+        print("\033[1;32;40mYour answer is correct")
     else :
-        print ("your answer is wrong, it's Reflex Angles")
-    print("Who discovered the laws of the lever and pulley?")
-    who = input()
+        print ("\033[1;31;40mYour answer is wrong, it's Reflex Angles")
+    print("\033[1;33;40mWho discovered the laws of the lever and pulley?")
+    who = input("===> ")
     if who== "Archimedes":
-        print("Your answer is true")
+        print("\033[1;32;40mYour answer is correct")
     else :
-        print("Your answer is wrong, it's Archimedes")
-    print("Scientist who was born on Pi Day?")
-    oo= input()
+        print("\033[1;31;40mYour answer is wrong, it's Archimedes")
+    print("\033[1;33;40mScientist who was born on Pi Day?")
+    oo= input("===> ")
     if oo=="Albert Einstein":
-        print("right answer")
+        print("\033[1;32;40mYour answer is correct")
     else:
-        print("your answer is wrong, it's Albert Einstein")
-    print("Who discovered the Symbol Infinity “∞”?")
-    ayo=input()
+        print("\033[1;31;40myour answer is wrong, it's Albert Einstein")
+    print("\033[1;33;40mWho discovered the Symbol Infinity “∞”?")
+    ayo = input("===> ")
     if ayo=="John Wallis":
-        print("right answer")
+        print("\033[1;31;40mYour answer is correct")
     else:
-        print("your answer is wrong, it's John Wallis")
-    print("Who invented Protractor?")
-    rr= input()
+        print("\033[1;31;40mYour answer is wrong, it's John Wallis")
+    print("\033[1;33;40mWho invented Protractor?")
+    rr= input("===> ")
     if rr=="Joseph Huddart":
-        print ("your answer is true")
+        print ("\033[1;32;40mYour answer is correct")
     else :
-        print ("your answer is wrong, it's Jose Huddart")
-    print (" Where was Abacus invented?")
-    kku=input()
+        print ("\033[1;31;40mYour answer is wrong, it's Jose Huddart")
+    print ("\033[1;33;40mWhere was Abacus invented?")
+    kku = input("===> ")
     if kku=="China":
-         print("true answer")
+         print("\033[1;32;40mYour answer is correct")
     else:
-         print("wrong answer, it's china")
+         print("\033[1;31;40mYour answer is wrong answer, it's china")
 elif choice == "Soocer":
-    print("FIFA stands for what?")
-    fifa = input()
-    if fifa == "Federation Initernationale de Football Association":
-        print("Your answer is correct.")
+    print("\033[1;33;40mFIFA stands for what?")
+    fifa = input("===> ")
+    if fifa == "\033[1;33;40mFederation Initernationale de Football Association":
+        print("\033[1;32;40mYour answer is correct.")
     else:
-        print("Your answer is wrong, the correct one is Federation Internationale de Fotball Association")
-    print("What is the number of players in 1 team of football?")
-    num = input()
-    if num == "11+ goal keeper" or "12":
-        print("Your answer is correct!")
+        print("\033[1;31;40mYour answer is wrong, the correct one is Federation Internationale de Fotball Association")
+    print("\033[1;33;40mWhat is the number of players in 1 team of football?")
+    num = input("===> ")
+    if num == "11+ goal keeper":
+        print("\033[1;32;40mYour answer is correct!")
     else:
-        print("Your answer is wrong, the correct one is 11 players with the goalkeeper")
-    print("In which country the headquarter of FA located?")
-    hf = input()
+        print("\033[1;31;40mYour answer is wrong, the correct one is 11 players with the goalkeeper")
+    print("\033[1;33;40mIn which country the headquarter of FA located?")
+    hf = input("===> ")
     if hf == "England":
-        print("Your answer is correct!")
+        print("\033[1;32;40mYour answer is correct!")
     else:
-        print("Your answer is wrong, the correct one is England")
-    print("Which country won the highest number of world cups?")
-    cup = input()
+        print("\033[1;31;40mYour answer is wrong, the correct one is England")
+    print("\033[1;33;40mWhich country won the highest number of world cups?")
+    cup = input("===> ")
     if cup == "Brazil":
-        print("Your answer is correct")
+        print("\033[1;32;40mYour answer is correct")
     else:
-        print("Your answer is wrong, the correct one is Brazil")
-    print("Bulgaria FIFA code is?")
-    code = input()
+        print("\033[1;31;40mYour answer is wrong, the correct one is Brazil")
+    print("\033[1;33;40mBulgaria FIFA code is?")
+    code = input("===> ")
     if code == "BUL":
+        print("\033[1;32;40mYour answer is correct")
+    else:
+        print("\033[1;31;40mYour answer is wrong, the correct one is BUL")
+    print("\033[1;33;40mWhich footballer has autobiography titled “A goal in the sky”?")
+    auto = input("===> ")
+    if auto == "Roberto Baggio":
+        print("\033[1;32;40mYour answer is correct")
+    else:
+        print("\033[1;31;40mYour answer is wrong, the correct one is Robert Baggio")
+    print("\033[1;33;40mWhat was the earlier form of soccer?")
+    soc = input("===> ")
+    if soc == "Socca":
+        print("\033[1;32;40mYour answer is correct")
+    else:
+        print("\033[1;31;40mYour answer is wrong, the correct one is Socca")
+    print("A player that has registred 5 goals in 9 mins?")
+    pl = input()
+    if pl == "Lewandowski":
         print("Your answer is correct")
     else:
-        print("Your answer is wrong, the correct one is BUL")
-    print("Which footballer has autobiography titled “A goal in the sky”?")
-    auto = input()
-    if auto == "Roberto Baggio" or "robert baggio" or "ROBERT BAGGIO":
-        print("Your answer is correct")
-    else:
-        print("Your answer is wrong, the correct one is Robert Baggio")
-    print("What was the earlier form of soccer?")
-    soc = input()
-    if soc == "Socca" or "socca" or "SOCCA":
-        print("Your answer is correct")
-    else:
-        print("Your answer is wrong, the correct one is Socca")
+         print("Your answer is wrong, it's Lewandowski")
+
+
